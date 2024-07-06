64ac8bc8c5134a8c25c2be06fc7fea91f31c4852
Khanh Chung <khanh.d.chung@gmail.com>
2024-07-06 13:56:38 -0700

    deploy to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
       manifest: {
         name: 'DonutMix',
         short_name: 'DonutMix',
